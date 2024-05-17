317caea21409e5d0358e987f2f4e53c4cdbbd6bb
Yan Victor Brito Gomes <ybritogomes@gmail.com>
2024-05-17 12:41:26 -0300

    feat(taskmodel): created task model and your respective migration, seeder and factory

---
+++ app/Models/Task.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory, HasUuids;
+
+            /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tasks';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+        /**
+     * The data type of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+}
